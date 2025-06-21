2342606f466042a208032a5613dd7bfc985fd678
sandy1983jsr <sandeep.uce208@gmail.com>
2025-06-21 12:25:56 +0530

    Update app.py

--- dashboard/app.py
+++ dashboard/app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 from value_pool_heatmap.heatmap import prepare_heatmap_data
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 st.title("ESG Growth Opportunities Dashboard")
 
