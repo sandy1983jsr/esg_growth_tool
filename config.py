d7c56b0857d674d33c7d4bd34e12cf454afc3d36
sandy1983jsr <sandeep.uce208@gmail.com>
2025-06-21 12:08:38 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+ESG_KEYWORDS = [
+    'sustainability', 'carbon neutral', 'net zero', 'renewable', 'diversity',
+    'inclusion', 'governance', 'decarbonization', 'climate risk', 'csr',
+    'social responsibility', 'emissions', 'green energy', 'circular economy'
+]
+
+SCRAPE_URLS = [
+    "https://www.reuters.com/business/sustainable-business/",
+    "https://www.esgtoday.com/category/environment/"
+]
+
+DATA_PATH = "data/"
